b97f52305c63fef9903538e15325877d9763aef6
zijx <zijianxiang@qq.com>
2016-01-28 10:01:02 +0800

    首页进展

--- PlanListView.js
+++ PlanListView.js
@@ -53,8 +53,59 @@ Log.id = 1;
 
 //========================数据类型 end========================
 var DefaultPlans = [new Plan('2016年度计划',1451577600,1483113600),new Plan('Plan 2',1451577600,1483113600)];
-var DefaultGoals = [new Goal('国内旅行',3,'次',1),new Goal('做家务',10,'次',1),new Goal('骑行',100,'公里',1),new Goal('',0,'',2)];
+var DefaultGoals = [new Goal('国内旅行',3,'次',1),new Goal('做家务',10,'次',1),new Goal('骑行',100,'公里',1)];
 var DefaultLogs = [new Log('五一桂林旅游',1,1),new Log('圣诞武汉游',1,1),new Log('扫地',1,2),new Log('周六清晨五缘湾骑行',15,3)];
+
+function makeDataBlob () {
+    var dataBlob = {};
+    var sectionIDs = [];
+    var rowIDs = [];
+
+    var addEmptyRow = (index, planId) => {
+	    var goal = new Goal('',0,'',0);
+  		var rowId = 'P '+planId+',R '+goal.id;
+        rowIDs[index].push(rowId);
+        dataBlob[rowId] = goal;
+	};
+
+    for (var ii = 0; ii < DefaultPlans.length; ii++) {
+    	var plan = DefaultPlans[ii];
+    	var sectionId = 'Plan '+plan.id;
+      	sectionIDs.push(sectionId);
+      	dataBlob[sectionId] = plan;
+      	rowIDs[ii] = [];
+      	
+      	if (plan.folded) {
+      		addEmptyRow(ii,plan.id);
+      	} else {
+	      	var goals = DefaultGoals.filter(function (goal) { return goal.planId === plan.id; });
+	      	// console.log('length of goals: '+goals.length+' planId: '+plan.id);
+	      	if (goals.length === 0) {
+	      		addEmptyRow(ii,plan.id);
+	      	};
+	      	for (var jj = 0; jj < goals.length; jj++) {
+	      		var goal = goals[jj];
+	      		var rowId = 'P '+plan.id+',R '+goal.id;
+		        rowIDs[ii].push(rowId);
+		        dataBlob[rowId] = goal;
+	      	}
+      	};
+    }
+    return [dataBlob,sectionIDs,rowIDs];
+}
+
+function formatDate (time,separator) {
+    var date = new Date(time*1000);
+    if (separator) {
+    	return date.getFullYear()+separator+(date.getMonth()+1)+separator+date.getDate();
+    } else {
+    	return date.getFullYear()+'年'+(date.getMonth()+1)+'月'+date.getDate()+'日';
+    };
+}
+
+function logsByGoalId (goalId) {
+	return DefaultLogs.filter(function (elog) { return elog.goalId === goalId; });
+}
 
 import React, {
 	Image,
@@ -139,48 +190,90 @@ export default class PlanListView extends Component {
 	      	sectionHeaderHasChanged: sectionHeaderHasChanged,
 	    });
 
-	    var dataBlob = {};
-	    var sectionIDs = [];
-	    var rowIDs = [];
-	    for (var ii = 0; ii < DefaultPlans.length; ii++) {
-	    	var plan = DefaultPlans[ii];
-	    	var sectionId = 'Section '+ii;
-	      	sectionIDs.push(sectionId);
-	      	dataBlob[sectionId] = plan;
-	      	rowIDs[ii] = [];
-	      	
-	      	var goals = DefaultGoals.filter(function (goal) { return goal.planId === plan.id; });
-	      	for (var jj = 0; jj < goals.length; jj++) {
-	      		var goal = goals[jj];
-	      		var rowId = 'S '+ii+',R '+jj;
-		        rowIDs[ii].push(rowId);
-		        dataBlob[rowId] = goal;
-	      	}
-	    }
+	    var results = makeDataBlob();
 	    this.state = {
-	      	dataSource: dataSource.cloneWithRowsAndSections(dataBlob, sectionIDs, rowIDs),
+	      	dataSource: dataSource.cloneWithRowsAndSections(results[0],results[1],results[2]),
+	      	dataBlob: results[0],
+	      	sectionIDs: results[1],
+	      	rowIDs: results[2],
 	    };
 	}
 
 	renderSectionHeader(sectionData, sectionID) {
 		console.log('renderSectionHeader '+sectionData.title);
-		return (<PlanHeader plan={sectionData}/>);
+      	if (sectionData.folded === true) {
+			return (
+				<TouchableOpacity onPress={() => {this._pressHeader(sectionData)}}>
+					<View>
+			    		<View style={styles.separator}/>	
+			    		<View style={styles.section}>
+			    			<Image source={require('./images/arrow-right.png')} style={styles.arrowRight} />
+			      			<Text style={styles.planTitle}>{sectionData.title}</Text>
+			      		</View>
+		      		</View>
+      			</TouchableOpacity>
+	    	);
+		} else {
+	    	return (
+	    		<TouchableOpacity onPress={() => {this._pressHeader(sectionData)}}>   
+	    			<View>     	
+			    		<View style={styles.separator}/>
+			    		<View style={styles.section}>
+			    			<Image source={require('./images/arrow-down.png')} style={styles.arrowDown} />
+			    			<View style={styles.container}>
+			      				<Text style={styles.planTitle}>{sectionData.title}</Text>
+			    				<Text style={styles.goalDesc}>{formatDate(sectionData.startTime)+'-'+formatDate(sectionData.endTime)}</Text>
+			    			</View>
+			      			<View style={styles.rightMenuContainer}>
+				      			<TouchableOpacity onPress={() => this._pressDelete(sectionData)}>
+				      				<Text style={styles.rightMenu}>删除</Text>
+				      			</TouchableOpacity>
+				      			<TouchableOpacity onPress={() => this._pressAddGoal(sectionData)}>
+				      				<Text style={styles.rightMenu}>添加</Text>
+				      			</TouchableOpacity>
+			      			</View>
+			      		</View>
+		      		</View>
+      			</TouchableOpacity>
+	    	);
+		};
   	}
 
-  	_pressHeader(sectionData,sectionID) {
+  	_pressHeader(sectionData) {
   		console.log('press header '+sectionData.folded);
   		sectionData.folded = !sectionData.folded;
   		var ds = this.state.dataSource;
-  		this.setState({dataSource:ds.cloneWithRowsAndSections(ds._dataBlob,ds.sectionIdentities,ds.rowIdentities)});
+  		var results = makeDataBlob();
+	    this.setState({
+	      	dataSource: ds.cloneWithRowsAndSections(results[0],results[1],results[2]),
+	    });
+  	}
+
+  	_pressDelete(sectionData) {
+
+  	}
+
+  	_pressAddGoal(sectionData) {
+  		
   	}
 
   	renderRow(rowData,sectionID,rowID) {
-  		console.log('renderRow '+rowData.desc);
-    	return (
-    		<View>
-      			<Text style={styles.goalDesc}>{rowData.desc}</Text>
-      		</View>
-    	);
+  		console.log('renderRow '+rowData.planId);
+  		if (rowData.planId > 0) {
+	    	return (
+	    		<View style={styles.goalContainer}>
+	    			<View style={{flex:1}}>
+	      				<Text style={styles.goalDesc}>{rowData.desc+' '+rowData.total+' '+rowData.unit}</Text>
+	    			</View>
+	    			<View style={styles.goalContainer}>
+	    				<Image source={require('./images/circle.png')} style={styles.goalButton} />
+	    				<Image source={require('./images/edit.png')} style={styles.goalButton} />
+	    			</View>
+	      		</View>
+	    	); 			
+  		} else {
+  			return (<View/>);
+  		}
   	}
 
 	render() {
@@ -203,9 +296,12 @@ var styles = StyleSheet.create({
     	backgroundColor: '#ffffff',
   	},
   	section: {
+  		flex: 1,
 	    flexDirection: 'row',
 	    alignItems: 'center',
-	    padding: 8,
+	    paddingHorizontal: 8,
+	    paddingVertical: 8,
+	    backgroundColor: '#f8f8f8',
 	},
 	arrowRight: {
 	    width: 5,
@@ -215,22 +311,54 @@ var styles = StyleSheet.create({
 	    width: 10,
 	    height: 5,
 	},
-  	planTitle : {
-    	fontSize : 16,
-    	color : '#000000',
-    	padding : 8,
+  	planTitle: {
+  		flex: 1,
+    	fontSize: 16,
+    	color: '#000000',
+    	paddingHorizontal: 8,
   	},
-  	goalDesc : {
-    	fontSize : 14,
-    	color : '#000000',
+	planPeriod: {
+		fontSize: 14,
+    	color: '#000000',
+    	paddingVertical: 5,
+    	paddingLeft: 8,
+	},
+	goalContainer: {
+		// flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+  	goalDesc: {
+    	fontSize: 14,
+    	color: '#000000',
+    	paddingVertical: 8,
+    	paddingLeft: 8,
   	},
-  	logDesc : {
-    	fontSize : 14,
-    	color : '#b8b8b8',
+  	goalButton: {
+  		width: 32,
+  		height: 32,
+  		margin: 8,
+  	},
+  	logDesc: {
+    	fontSize: 14,
+    	color: '#b8b8b8',
   	},
   	separator: {
-	    height: StyleSheet.hairlineWidth,
-	    backgroundColor: '#bbbbbb',
+	    height: 1,
+	    backgroundColor: '#ffffff',
 	    marginLeft: 0,
+  	},
+  	container: {
+  		flex: 1,
+  	},
+  	rightMenuContainer: {
+  		flexDirection: 'row',
+	    justifyContent: 'flex-end',
+	    // backgroundColor: '#ff0000',
+  	},
+  	rightMenu: {
+  		fontSize: 14,
+  		color: '#8080d0',
+	    paddingHorizontal: 8,
   	},
 });
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # iplan
 一个用react-native实现的制定计划、跟进进度的app，只有ios端
+注意：
+下载以后拷贝其它通过react-native init生成的项目中的node_modules目录过来即可运行（切记：不是react-native自身的node_modules目录）
